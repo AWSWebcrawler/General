b3c5598dcb442653262e5e1daee183dab183183d
jah4511 - Jan-Erik Herrmann <102309364+jah4511@users.noreply.github.com>
2022-04-29 20:38:41 +0200

    Issue #70 deleted code which was in wrong branch
    Have commited the code to the wrong branch. Had to delete it

--- crawler/log/logging_config.py
+++ crawler/log/logging_config.py
@@ -1,11 +1,1 @@
-import logging
-from datetime import date
 
-file = '../log/log_output/' + str(date.today()) + '.log'
-fmtstr = " %(asctime)s: (%(filename)s): %(levelname)s: %(funcName)s Line: %(lineno)d - %(message)s"
-datestr = "%m/%d/%Y %I:%M:%S %p "
-logging.basicConfig(filename=file,
-                    level=logging.INFO,
-                    filemode='a',
-                    format=fmtstr,
-                    datefmt=datestr)
