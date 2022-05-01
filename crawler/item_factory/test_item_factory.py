f726af8ba4de5d2cf2610f29761b289005396bdc
DESKTOP-N9LJ6CT\Alexander Horn <alh5598@thi.de>
2022-05-01 17:01:42 +0200

    Issue#74: Moving Test Files to Test Directory.

--- crawler/item_factory/test_item_factory.py
+++
@@ -1,8 +0,0 @@
-from item_factory import create_item
-
-html= ""
-with open('html.txt', 'r') as file:
-    html = file.read()
-
-item = create_item(html)
-print(item)
--- crawler/tests/test_item_factory.py
+++ crawler/tests/test_item_factory.py
@@ -1,1 +1,8 @@
-#example test script
+from ..item_factory.item_factory import create_item
+
+html= ""
+with open('../item_factory/html.txt', 'r') as file:
+    html = file.read()
+
+item = create_item(html)
+print(item)
