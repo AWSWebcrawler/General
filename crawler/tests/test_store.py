d10d0ae19fc5a5ff7923ee5ffbcecc16779304df
JanErikHerrmann <jah4511@thi.de>
2022-05-02 20:03:56 +0200

    issue #75 created store module and test
    Store module takes a dictionary and creates a csv file. store.py already contains a potential method to determine if the script is called in aws.

--- crawler/tests/test_store.py
+++ crawler/tests/test_store.py
@@ -1,8 +1,29 @@
-#example test script
+# example test script
+import crawler.store.store as st
+import csv
+
+
 def test_store_to_csv():
     """ Möglicher Test:
         - Anlegen eines Items
         - aufrufen der Methode mit Item und tmp-Datei (im tests ordner)
         - Einlesen der letzten Zeile der tmp-Datei
         - Assertion ob Werte aus Datei mit den Werten der Items übereinstimmen"""
-    pass
+    sample_dict = {"name": "python", "version": 3.9}
+    st.store_item(sample_dict)
+    filename = 'test4'
+    with open('..\\output\\' + filename + '.csv', newline='') as f:
+        csvFile = csv.reader(f)
+        complete = ''
+        for line in csvFile:
+            complete = str(complete)+str(line)
+            print(complete)
+
+    f.close()
+    #Need to append other rows/ lines if tested differently
+    assert complete == "['1', '2', '3']['1', '2', '3']['1', '2', '3']['1', '2', '3']", 'Map of csv does not math with expected result'
+
+
+
+
+test_store_to_csv()
