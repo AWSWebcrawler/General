9a934257facc330c5646dd0256201b39fde7599f
DESKTOP-N9LJ6CT\Alexander Horn <alh5598@thi.de>
2022-05-07 08:37:05 +0200

    Issue#74: Adding the url as Parameter to function create_item of item_factory. Implementation of the functions get_price_regular, get_percent_discount, get_discount_in_euros and refactoring of functions get_name, get_selled_by_amazon, get_seller, get_price and get_url.

--- crawler/tests/test_item_factory.py
+++ crawler/tests/test_item_factory.py
@@ -1,8 +1,9 @@
 from crawler.item_factory import item_factory
 
+url="https://www.amazon.de/dp/B084DWG2VQ?ref_=cm_sw_r_cp_ud_dp_93J9GCGXC997VW26Y0ES"
 html= ""
-with open('./html.txt', 'r') as file:
+with open('./html.txt', 'r', encoding='utf8') as file:
     html = file.read()
 
-item = item_factory.create_item(html)
+item = item_factory.create_item(html,url)
 print(item)
