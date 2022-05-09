aaeb8d5d9e23a06f040577377b87c05d49deff2c
jah4511 - Jan-Erik Herrmann <102309364+jah4511@users.noreply.github.com>
2022-05-09 15:50:32 +0200

    #issue 94 changed the expected value
    Changed expected value to math the new headers in the store module

--- crawler/tests/test_store.py
+++ crawler/tests/test_store.py
@@ -10,11 +10,10 @@ import os
             - Einlesen der letzten Zeile der tmp-Datei
             - Assertion ob Werte aus Datei mit den Werten der Items übereinstimmen"""
 
-
 class test_store(unittest.TestCase):
     def test_store_to_csv(self):
         sample_dict = {"name": "python", "version": 3.9}
-        expected = "['header1', 'header2']['python', '3.9']"
+        expected = "['timestamp', 'date', 'time', 'name', 'product_id', 'price', 'price_regular', 'prime', 'discount in euros', 'percent discount', 'sold_by_amazon', 'seller', 'asin', 'url']['python', '3.9']"
         st.store_item(sample_dict)
         filename = 'test4'
 
@@ -26,4 +25,4 @@ class test_store(unittest.TestCase):
 
             f.close()
             os.remove('..\\output\\' + filename + '.csv')
-            self.assertEqual(complete, expected, "The result and the expected value were not compliant. Please refer to the difference log in the run terminal")
+            self.assertEqual(expected, complete, "The result and the expected value were not compliant. Please refer to the difference log in the run terminal")
