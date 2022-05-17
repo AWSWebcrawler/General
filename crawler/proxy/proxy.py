4054da924ac0bd4cdd21e9728e226e645f521d0c
DESKTOP-N9LJ6CT\Alexander Horn <alh5598@thi.de>
2022-05-17 21:10:53 +0200

    Refactoring of function proxy. Global Proxy list with proxies of 3 files. Not working proxies will be removed. Socks4 and Socks5 proxies are used too. The proxies are chosen randomly.

--- crawler/proxy/proxy.py
+++ crawler/proxy/proxy.py
@@ -1,76 +1,78 @@
 import logging
 import requests
-from lxml import etree
-from io import StringIO
-
+from random import choice
 """The proxy module gets an url and a dictionary for the headers and returns a functioning proxy"""
 
+PROXY_LIST = None
+proxy_urls = {
+    "socks4" : "https://raw.githubusercontent.com/saschazesiger/Free-Proxies/94732c66982abfc273cfb41056efe7a062b78d01/proxies/socks4.txt",
+    "socks5" : "https://raw.githubusercontent.com/saschazesiger/Free-Proxies/94732c66982abfc273cfb41056efe7a062b78d01/proxies/socks5.txt",
+    "http" : "https://raw.githubusercontent.com/saschazesiger/Free-Proxies/94732c66982abfc273cfb41056efe7a062b78d01/proxies/http.txt"
+}
 
-def get_proxy(url, header):
-    """Calls the implemented functions and returns proxy"""
+def get_html(url, header, old_proxy) -> dict:
 
-    logging.debug("Calling function get_proxy")
+    if old_proxy is None:
+        proxy = get_random_proxy()
+        return call_url(url, header, proxy)
 
-    list_of_proxies = get_proxies()
-    proxy = get_working_proxy(list_of_proxies, url, header)
-    return proxy
+    return call_url(url, header, old_proxy)
 
+def call_url(url, header, proxy) -> dict:
+    try:
+        response = requests.get(url, headers=header, proxies=proxy, timeout=3, verify=False)
+        if(response.status_code == 200 and "(MEOW)" in response.text):
+            html_with_proxy = {
+                'html' : response.text,
+                'proxy' : proxy
+            }
+            return html_with_proxy
+        logging.error("Timeout")
+    except:
+        logging.error("Proxy not working")
+
+    remove_proxy_from_list(proxy)
+    return call_url(url, header, get_random_proxy())
 
 def get_proxies():
     """Creates a list of all proxies listed in the txt file of the proxy_url"""
 
+    global PROXY_LIST
     logging.debug("Calling function get_proxies")
 
-    proxy_url = "https://github.com/saschazesiger/Free-Proxies/blob/94732c66982abfc273cfb41056efe7a062b78d01/proxies/ultrafast.txt"
+    response_socks4 = requests.get(proxy_urls["socks4"])
+    response_socks5 = requests.get(proxy_urls["socks5"])
+    response_http = requests.get(proxy_urls["http"])
 
-    response = requests.get(proxy_url)
+    socks4_proxies = [row.decode() for row in response_socks4.iter_lines()]
+    socks5_proxies = [row.decode() for row in response_socks5.iter_lines()]
+    http_proxies = [row.decode() for row in response_http.iter_lines()]
 
-    parser = etree.HTMLParser()
-    tree = etree.parse(StringIO(response.text), parser)
+    socks4_proxies = ["socks4://"+proxy for proxy in socks4_proxies]
+    socks5_proxies = ["socks5h://" + proxy for proxy in socks5_proxies]
 
-    list_of_proxies = tree.findall('.//td[@class = "blob-code blob-code-inner js-file-line"]')
-    list_of_proxies = [proxy.text for proxy in list_of_proxies]
+    PROXY_LIST = http_proxies
+    PROXY_LIST.extend(socks4_proxies)
+    PROXY_LIST.extend(socks5_proxies)
 
-    return list_of_proxies
-
-
-def get_working_proxy(list_of_proxies, url, header):
-    """Calls the test methode till a functional proxy is found or all proxies are tested"""
-
-    logging.debug("Calling function get_working_proxy")
-
-    count = 0
-    proxy = None
-
-    while count < len(list_of_proxies) and proxy is None:
-        proxy = test_proxy(list_of_proxies, count, url, header)
-        count += 1
-
-    if proxy is not None:
-        return proxy
-    else:
-        logging.error("No working proxy found")
-        # raise no_working_proxy_found_Exception()
-
-
-def test_proxy(list_of_proxies, count, url, header):
+def get_random_proxy() -> dict:
     """Tests the proxy with the given header and url"""
 
     logging.debug("Calling function test_proxy")
 
-    proxy_domain = list_of_proxies[count]
+    if PROXY_LIST is None:
+        get_proxies()
+
+    proxy_ip = choice(PROXY_LIST)
+
     proxy = {
-        "https": proxy_domain
+        "http": proxy_ip
     }
+    print(proxy)
+    return proxy
 
-    try:
-        logging.debug("Testing proxy:" + proxy["https"])
-        # print("Testing proxy:" + proxy["https"])
-        response = requests.get(url, headers=header, proxies=proxy, timeout=5)
-
-        if response.status_code == 200:
-            logging.debug("functioning proxy found")
-            return proxy
-
-    except:
-        return None
+def remove_proxy_from_list(proxy):
+    proxy = proxy['http']
+    if PROXY_LIST is not None and proxy in PROXY_LIST:
+        PROXY_LIST.remove(proxy)
+        print(len(PROXY_LIST))
--- crawler/tests/test_proxy.py
+++ crawler/tests/test_proxy.py
@@ -1,8 +1,8 @@
-from crawler.proxy.proxy import get_proxy
+from crawler.proxy.proxy import get_html
 import requests
 
 header_dict = {
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36",
+    "user-agent": 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:38.0) Gecko/20100101 Firefox/38.0',
     "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,"
               "*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
     "accept-encoding": "gzip, deflate, br",
@@ -11,14 +11,23 @@ header_dict = {
     'Connection': 'keep-alive'}
 
 # proxy = {
-#     "https" : "147.75.88.40:80"
-#     "https": "147.75.88.40:10001"
+#     "http": "203.24.109.115:80",
+#     "https" : "203.24.109.115:80"
+#
+# }
+# proxy = {
+#     "http": 'socks5h://72.217.216.239:4145',
+#     "https": 'socks5h://72.217.216.239:4145'
 # }
 
-proxy = get_proxy("https://www.amazon.de/Zertifiziert-general%C3%BCberholt-Display-HD-Bildschirm-Anthrazit/dp/B07SNPP6P3", header_dict)
-
-response = requests.get("https://www.amazon.de/Zertifiziert-general%C3%BCberholt-Display-HD-Bildschirm-Anthrazit/dp/B07SNPP6P3", headers=header_dict, proxies=proxy)
-print(response.text)
-print(response.status_code)
-print("Proxy:" + proxy["https"])
-print(response.headers)
+html_with_proxy = get_html("http://www.amazon.de/Garmin-Venu-GPS-Fitness-Smartwatch-AMOLED-Touchdisplay-Gesundheitsfunktionen/dp/B091ZXYQXF", header_dict, None)
+print(html_with_proxy["html"])
+print(html_with_proxy["proxy"])
+html_with_proxy = get_html("http://www.amazon.de/Apple-iPhone-Pro-Max-128-GB/dp/B09G91T787", header_dict, html_with_proxy["proxy"])
+print(html_with_proxy["html"])
+print(html_with_proxy["proxy"])
+# response = requests.get("https://www.amazon.de/Garmin-Venu-GPS-Fitness-Smartwatch-AMOLED-Touchdisplay-Gesundheitsfunktionen/dp/B091ZXYQXF", headers=header_dict, proxies=proxy, timeout=5, verify=False)
+# print(response.text)
+# print(response.status_code)
+# print("Proxy:" + proxy["https"])
+# print(response.headers)
