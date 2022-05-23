e22af61d5391d3880febff9a85b4d82feaf5c4ce
DESKTOP-N9LJ6CT\Alexander Horn <alh5598@thi.de>
2022-05-23 19:59:16 +0200

    Issue#132: Refactoring the Item_factory as discussed in the code review. Updated the test html file. Adapting the test for the item factory to new values resulting from the refactoring.

--- tests/test_item_factory.py
+++ tests/test_item_factory.py
@@ -21,5 +21,5 @@ class TestItemFactory(unittest.TestCase):
         product["date"] = None
         product["timestamp"] = None
 
-        expected = "{'name': 'Echo Dot (4. Generation) | Smarter Lautsprecher mit Alexa | Anthrazit', 'current_price': 59.99, 'price_regular': 59.99, 'prime': True, 'discount_in_euros': 0.0, 'percent_discount': 0.0, 'sold_by_amazon': True, 'seller': 'Amazon', 'amazon_choice': True, 'asin': 'B084DWG2VQ', 'url': 'https://www.amazon.de/dp/B084DWG2VQ?ref_=cm_sw_r_cp_ud_dp_93J9GCGXC997VW26Y0ES', 'timestamp': None, 'date': None, 'time': None}"
+        expected = "{'name': 'Echo Dot (4. Generation) | Smarter Lautsprecher mit Alexa | Anthrazit', 'current_price': 59.99, 'price_regular': 59.99, 'prime': True, 'discount_in_euros': None, 'percent_discount': None, 'sold_by_amazon': True, 'seller': 'Amazon', 'amazon_choice': True, 'asin': 'B084DWG2VQ', 'url': 'https://www.amazon.de/dp/B084DWG2VQ?ref_=cm_sw_r_cp_ud_dp_93J9GCGXC997VW26Y0ES', 'timestamp': None, 'date': None, 'time': None}"
         self.assertEqual(expected, str(product), "The created product does not match the expected output.")
