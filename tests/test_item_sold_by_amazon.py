b2df614b82d4c4a28096b86cb3a5a0027b20ef61
DESKTOP-N9LJ6CT\Alexander Horn <alh5598@thi.de>
2022-06-17 16:29:03 +0200

    Issue#162: Refactoring the tests for items of the item factory.

--- tests/test_item_sold_by_amazon.py
+++ tests/test_item_sold_by_amazon.py
@@ -9,45 +9,71 @@ class TestItemFactory(unittest.TestCase):
 
     def setUp(self) -> None:
         self.urls = {
-            'url1': 'https://www.amazon.de/der-neue-echo-dot-4-generation-smarter-lautsprecher-mit-alexa-anthrazit/' \
-                    'dp/B084DWG2VQ',
-            'url2': 'https://www.amazon.de/Xbox-Wireless-Controller-Electric-Volt/dp/B091CK241X',
-            'url3': 'https://www.amazon.de/FLAMMBURO-Paraffinbasis-Grillanz%C3%BCnder-Kaminanz%C3%BCnder-Paraffinw' \
-                    '%C3%BCrfel/dp/B08YCWDLTQ',
-            'url4': 'https://www.amazon.de/CYBERPUNK-2077-DAY-Standard-Xbox/dp/B07SF1LZ9Q',
+            "url1": "https://www.amazon.de/der-neue-echo-dot-4-generation-smarter-lautsprecher-mit-alexa-anthrazit/"
+            "dp/B084DWG2VQ",
+            "url2": "https://www.amazon.de/Xbox-Wireless-Controller-Electric-Volt/dp/B091CK241X",
+            "url3": "https://www.amazon.de/FLAMMBURO-Paraffinbasis-Grillanz%C3%BCnder-Kaminanz%C3%BCnder-Paraffinw"
+            "%C3%BCrfel/dp/B08YCWDLTQ",
+            "url4": "https://www.amazon.de/CYBERPUNK-2077-DAY-Standard-Xbox/dp/B07SF1LZ9Q",
         }
 
-        with open('./test_item_factory_testfile1.html', 'r', encoding='utf8') as file:
+        with open("./test_item_factory_testfile1.html", "r", encoding="utf8") as file:
             test_html_1 = file.read()
-        with open('./test_item_factory_testfile2.html', 'r', encoding='utf8') as file:
+        with open("./test_item_factory_testfile2.html", "r", encoding="utf8") as file:
             test_html_2 = file.read()
-        with open('./test_item_factory_testfile3.html', 'r', encoding='utf8') as file:
+        with open("./test_item_factory_testfile3.html", "r", encoding="utf8") as file:
             test_html_3 = file.read()
-        with open('./test_item_factory_testfile4.html', 'r', encoding='utf8') as file:
+        with open("./test_item_factory_testfile4.html", "r", encoding="utf8") as file:
             test_html_4 = file.read()
 
         self.test_html = {
-            'test_html_1': test_html_1,
-            'test_html_2': test_html_2,
-            'test_html_3': test_html_3,
-            'test_html_4': test_html_4,
+            "test_html_1": test_html_1,
+            "test_html_2": test_html_2,
+            "test_html_3": test_html_3,
+            "test_html_4": test_html_4,
         }
+
+        self.function_name_with_html = {}
 
     def test_create_item(self):
         """Tests the create item function of the item_factory module"""
 
-        product = item_factory.create_item(self.test_html['test_html_1'], self.urls['url1'])
-        self.assertTrue(product["sold_by_amazon"],
-                        "The created item sold_by_amazon does not match the expected output.")
+        product = item_factory.create_item(
+            self.test_html["test_html_1"],
+            self.urls["url1"],
+            self.function_name_with_html,
+        )
+        self.assertTrue(
+            product["sold_by_amazon"],
+            "The created item sold_by_amazon does not match the expected output.",
+        )
 
-        product = item_factory.create_item(self.test_html['test_html_2'], self.urls['url2'])
-        self.assertFalse(product["sold_by_amazon"],
-                         "The created item sold_by_amazon does not match the expected output.")
+        product = item_factory.create_item(
+            self.test_html["test_html_2"],
+            self.urls["url2"],
+            self.function_name_with_html,
+        )
+        self.assertFalse(
+            product["sold_by_amazon"],
+            "The created item sold_by_amazon does not match the expected output.",
+        )
 
-        product = item_factory.create_item(self.test_html['test_html_3'], self.urls['url3'])
-        self.assertFalse(product["sold_by_amazon"],
-                         "The created item sold_by_amazon does not match the expected output.")
+        product = item_factory.create_item(
+            self.test_html["test_html_3"],
+            self.urls["url3"],
+            self.function_name_with_html,
+        )
+        self.assertFalse(
+            product["sold_by_amazon"],
+            "The created item sold_by_amazon does not match the expected output.",
+        )
 
-        product = item_factory.create_item(self.test_html['test_html_4'], self.urls['url4'])
-        self.assertFalse(product["sold_by_amazon"],
-                         "The created item sold_by_amazon does not match the expected output.")
+        product = item_factory.create_item(
+            self.test_html["test_html_4"],
+            self.urls["url4"],
+            self.function_name_with_html,
+        )
+        self.assertFalse(
+            product["sold_by_amazon"],
+            "The created item sold_by_amazon does not match the expected output.",
+        )
